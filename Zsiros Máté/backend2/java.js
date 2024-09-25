87735a736248d3ff3c1514ed7c925e417c09d3be
VidekiCorleone <144677644+VidekiCorleone@users.noreply.github.com>
2024-09-25 10:34:06 +0200

    asdasd

--- Zsiros Máté/backend2/java.js
+++ Zsiros Máté/backend2/java.js
@@ -1,4 +1,4 @@
-// let és const
+/*// let és const
 // es5
 
 var nev5 = 'Teszt Elek';
@@ -59,7 +59,7 @@ console.log(a+b);
     console.log(c);
 })();
 console.log(c)*/
-
+/*
 let vezetekNev = 'Teszt';
 let keresztNev = 'Elek';
 
@@ -99,12 +99,154 @@ console.log(korokES6);
 
 korokES6 = evek.map((elem, index) => `Kor ${index}: ${2024 - elem}.`);
 
-console.log(korokES6)
+console.log(korokES6)*/
 
-korokES6 = evek.map((elem, index) => {
+/*korokES6 = evek.map((elem, index) => {
     const aktEv = new Data().getFullYear();
     const kor = aktEv - elem;
     retunr `Kor ${index}: ${kor},`;
 });
 
-console.log(korokES6)
+console.log(korokES6)*/
+
+/*var dobozES5 = {
+    szin: 'zöld',
+    pozicio: 1,
+    kattintsRam: function() {
+        var objektum = this;
+
+        document.querySelector('.green').addEventListener('click', function(){
+            var szoveg = 'Én vagyok a(z) ' + objektum.pozicio + '. doboz és a szinem ' + objektum.szin + '.';
+            alert(szoveg)
+        })
+    }
+};
+
+dobozES5.kattintsRam();*/
+
+/*var dobozES6 = {
+    szin: 'zöld',
+    pozicio: 1,
+    kattintsRam: function() {
+
+        document.querySelector('.green').addEventListener('click', () => {
+            var szoveg = 'Én vagyok a(z) ' + this.pozicio + '. doboz és a szinem ' + this.szin + '.';
+            alert(szoveg)
+        })
+    }
+};
+
+dobozES6.kattintsRam()
+
+function Szemely(nev) {
+    this.nev = nev;
+}
+
+Szemely.prototype.barataimES5 = function(haverok) {
+    var obj = this
+    var tomb = haverok.map(function(elem) {
+        return obj.nev + ' barátja' + elem + '.';
+    });
+    console.log(tomb)
+}
+
+var haverok = ['Jóska', 'Pista', 'Ödön'];
+new Szemely('Géza').barataimES5(haverok);
+
+//ES6
+
+Szemely.prototype.barataimES6 = function(haverok) {
+
+    const tomb = haverok.map(elem => `${this.nve} baratja ${elem}.`);
+    console.log(tomb);
+}
+
+
+var haverok = ['Jóska', 'Pista', 'Ödön'];
+new Szemely('Géze').barataimES6(haverok);
+
+//es5 
+var odon = ['Ödön', 50]
+var nev = odon[0];
+var kor = odon[1];
+
+console.log(nev);
+console.log(kor);
+
+//es6
+const [nev2, kor2] = ['Ödön', 50];
+
+console.log(nev);
+console.log(kor);
+
+const obj = {
+    keresztNev: 'Ödön',
+    vezetekNev: 'Bödön'
+};
+
+const { keresztNev: x, vezetekNev: y} = obj;
+
+console.log(x);
+console.log(y);
+
+function korEsNyugdij(szuletesiEv) {
+    let nyugdijKorhatar = 65;
+    const kor = new Date().getFullYear() - szuletesiEv;
+
+    return [kor, nyugdijKorhatar - kor];
+}*/
+
+const dobozok = document.querySelectorAll('.rectangle');
+
+// ES5
+var dobozokTombES5 = Array.prototype.slice.call(dobozok);
+dobozokTombES5.forEach(function(aktualis) {
+    aktualis.style.backgroundColor = 'orange';
+});
+
+// ES6
+const dobozokTombES6 = Array.from(dobozok);
+dobozokTombES6.forEach(aktualis => aktualis.style.backgroundColor = 'blue');
+
+//es5
+
+for (var i = 0; i < dobozokTombES5.length; i++) {
+    if (dobozokTombES5[i].className === 'rectangle blue' ) {
+        continue;
+    }
+    dobozokTombES5[i].textContent = 'Kék lettem';
+}
+
+
+//es6
+
+for (const aktualis of dobozokTombES6) {
+    if (aktualis.className === 'rectangle blue') {
+        continue;
+    }
+    aktualis.textContent = 'Kék lettem';
+}
+
+for (const aktualis of dobozokTombES6) {
+    if (aktualis.className.includes('blue')) {
+        continue;
+    }
+    aktualis.textContent = 'Kék lettem';
+}
+
+//es5
+var korok = [2, 10, 20, 17, 14]
+
+console.log(korok);
+
+var felnottek = korok.map(function(aktualis) {
+    return aktualis >= 18;
+});
+
+//console.log(felnottek);
+
+console.log(felnottek.indexOf(true));
+
+//es6
+console.log(korok.findIndex(aktualis => aktualis >= 18));
+console.log(korok.find(aktualis => aktualis >= 18));
