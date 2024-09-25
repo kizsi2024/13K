66091c3d874dcad20603c11972519a86627f3d65
Telepes Street 105 <144677555+Bencee130@users.noreply.github.com>
2024-09-25 10:35:13 +0200

    Add files via upload

---
+++ Tóth Bence/backend/java.js
@@ -0,0 +1,246 @@
+// let és const
+// es5
+
+var nev5 = 'Teszt Elek';
+var kor5 = 40;
+
+//nev5 = 'Kiss Pista';
+
+/*console.log(nev5);
+
+function nyelvVizsga5(siker) {
+    if (siker) {
+        var keresztnev = 'Ödön';
+        var szuletesiEv = 1980
+        console.log(keresztnev + '(születési év: ' + szuletesiEv + ') sikeres volt a vizsga');
+    }
+}
+
+nyelvVizsga5(true);
+
+const nev6 = 'Teszt Elek';
+let kor6 = 40;
+
+function nyelvVizsga6(siker) {
+    if (siker) {
+        var keresztnev = 'Ödön';
+        var szuletesiEv = 1980
+        console.log(keresztnev + '(születési év: ' + szuletesiEv + ') sikeres volt a vizsga');
+    }
+}
+
+nyelvVizsga6(true);
+
+let i = 9;
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+console.log(i);
+
+
+
+var i = 9;
+for (var i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+console.log(i);
+
+{
+    const a = 1;
+    let b = 2;
+}
+
+console.log(a+b);
+
+(function() {
+    let c = 3;
+    console.log(c);
+})();
+console.log(c)*/
+
+/*let vezetekNev = 'Teszt';
+let keresztNev = 'Elek';
+
+const szuletesiEv = 1970;
+
+function korSzamintas(ev){
+    return 2024 - ev;
+}
+
+console.log(vezetekNev + ' ' + keresztNev + ',született ' + szuletesiEv + '-ben. Most ' + korSzamintas(szuletesiEv) + ' éves.');
+
+console.log(`${vezetekNev} ${keresztNev}, született ${szuletesiEv}-ben. Most ${korSzamintas(szuletesiEv)} éves.`)
+
+const nev = `${vezetekNev} ${keresztNev}`
+console.log(nev.startsWith('T'));
+console.log(nev.endsWith('ek'))
+console.log(nev.includes('ele'))
+console.log(`${vezetekNev} `.repeat(3))
+
+//Nyíl függvény
+
+const evek = [1970, 1975, 1954, 2010, 1981];
+
+// es5
+
+var korokES5 = evek.map(function(elem) {
+    return 2024 - elem;
+});
+
+console.log(korokES5);
+
+// es6
+
+let korokES6 = evek.map(elem => 2024 - elem);
+
+console.log(korokES6);
+
+korokES6 = evek.map((elem, index) => `Kor ${index}: ${2024 - elem}.`);
+
+console.log(korokES6)*/
+
+/*korokES6 = evek.map((elem, index) => {
+    const aktEv = new Data().getFullYear();
+    const kor = aktEv - elem;
+    retunr `Kor ${index}: ${kor},`;
+});
+
+console.log(korokES6)*/
+
+/*var dobozES5 = {
+    szin: 'zöld',
+    pozicio: 1,
+    kattintsRam: function() {
+        var objektum = this;
+
+        document.querySelector('.green').addEventListener('click', function(){
+            var szoveg = 'Én vagyok a(z) ' + objektum.pozicio + '. doboz és a szinem ' + objektum.szin + '.';
+            alert(szoveg)
+        })
+    }
+};
+
+dobozES5.kattintsRam();*/
+
+
+/*var dobozES6 = {
+    szin: 'zöld',
+    pozicio: 1,
+    kattintsRam: function() {
+        var objektum = this;
+
+        document.querySelector('.green').addEventListener('click',() => {
+            var szoveg = 'Én vagyok a(z) ' + this.pozicio + '. doboz és a szinem ' + objektum.szin + '.';
+            alert(szoveg)
+        })
+    }
+};
+
+dobozES6.kattintsRam();*/
+
+/*function Szemely(nev) {
+    this.nev = nev;
+}
+
+Szemely.prototype.barataimES5 = function(haverok) {
+    var obj = this
+    var tomb = haverok.map(function(elem) {
+        return obj.nev + ' barátja' + elem + '.';
+    });
+    console.log(tomb)
+}
+
+var haverok = ['Jóska', 'Pista', 'Ödön'];
+new Szemely('Géza').barataimES5(haverok);*/
+
+
+//ES6
+
+/*Szemely.prototype.barataimES6 = function(haverok) {
+
+    const tomb = haverok.map(elem => `${this.nve} baratja ${elem}.`);
+    console.log(tomb);
+}
+
+
+var haverok = ['Jóska', 'Pista', 'Ödön'];
+new Szemely('Géze').barataimES6(haverok);*/
+
+
+//es5 
+/*
+var odon = ['Ödön', 50]
+var nev = odon[0];
+var kor = odon[1];
+
+console.log(nev);
+console.log(kor);
+
+//es6
+const [nev2, kor2] = ['Ödön', 50];
+
+const obj = {
+    keresztNev: 'Ödön',
+    vezetekNev: 'Bödön'
+};
+
+const { keresztNev: x, vezetekNev: y} = obj;
+
+console.log(x);
+console.log(y);
+
+function korEsNyugdij(szuletesiEv) {
+    let nyugdijKorhatar = 65;
+    const kor = new Date().getFullYear() - szuletesiEv;
+
+    return [kor, nyugdijKorhatar - kor];
+}
+
+const [kor3, nyugdij] = korEsNyugdij(1978);
+
+console.log(kor3);
+console.log(nyugdij);*/
+
+//Tömbök
+
+const dobozok = document.querySelectorAll('.rectangle');
+
+//es5
+
+var dobozokTombES5 = Array.prototype.slice.call(dobozok)
+    dobozokTombES5.forEach(function(aktualis) {
+        aktualis.style.backgroundColor = 'orange' ;
+    });
+    
+
+//es6
+
+const dobozokTombES6 = Array.from(dobozok);
+dobozokTombES6.forEach(aktualis => aktualis.style.backgroundColor = 'blue');
+
+//es5
+
+for (var i = 0; i < dobozokTombES5.length; i++) {
+    if (dobozokTombES5[i].className === 'rectangle blue' ) {
+        continue;
+    }
+    dobozokTombES5[i].textContent = 'Kék lettem';
+}
+
+//es6
+
+for (const aktualis of dobozokTombES6) {
+    if (aktualis.className === 'rectangle blue') {
+        continue;
+    }
+    aktualis.textContent = 'Kék lettem';
+}
+
+for (const aktualis of dobozokTombES6) {
+    if (aktualis.className.includes('blue')) {
+        continue;
+    }
+    aktualis.textContent = 'Kék lettem';
+}
