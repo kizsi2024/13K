129e46bad12573b239aae8ce29920b890858893a
Kovacs Zeteny <kovacs.zeteny19k@egressy.info>
2024-11-13 12:34:27 +0100

    aaa

--- Kovács Zétény/cyberdyne-system/src/App.js
+++ Kovács Zétény/cyberdyne-system/src/App.js
@@ -1,9 +1,76 @@
 import logo from './logo.svg';
 import './App.css';
-import React, {useState } from 'react';
+import React, {Component } from 'react';
 import Person from './Person/Person'
 
 
+class App extends Component{
+  state={
+    persons: [
+    {name: "Valaki", age:28},
+    {name: "Dr Lilla ", age:40},
+    {name: "Papa", age:55}
+  ]
+  }
+  nameChangedHandler = (ujNev) => {
+  
+  this.setState({
+    persons: [
+      {name: ujNev, age:28},
+      {name: "Dr Lilla ", age:40},
+      {name: "Papa", age:55}
+    ]
+  })
+  }
+nevValtozasHandler =(event) =>{
+  this.setState({
+    persons: [
+      {name:event.target.value, age:28},
+      {name:event.target.value, age:40},
+      {name:event.target.value, age:55}
+    ]
+  })
+}
+
+  render(){
+    return (
+      <div className='App'>
+        <h1> React feladatok</h1>
+        <p> Ez egy parapgrafus </p>
+        <button onClick={this.nameChangedHandler.bind(this,'teszt')}>Nevet módosit</button>
+        <Person
+        name={this.state.persons[0].name}
+        age={this.state.persons[0].age}
+        click={this.nameChangedHandler.bind(this, 'Hübele Bazsi')}
+        change={this.nevValtozasHandler}
+        />
+        <Person
+        name={this.state.persons[1].name}
+        age={this.state.persons[1].age}
+        click={()=>this.nameChangedHandler('Csak Balázs')}
+        change={this.nevValtozasHandler}
+        />
+        <Person
+        name={this.state.persons[2].name}
+        age={this.state.persons[2].age}
+        change={this.nevValtozasHandler}
+        />
+  
+        VAlami Amit máté mondOT
+      </div>
+    )
+  
+}
+}
+
+export default App;
+
+
+  
+//console.log(this.state)
+
+
+/*
 const App = props =>{
   const[aktulisState, ujState] = useState({
     persons: [
@@ -40,17 +107,17 @@ return(
       VAlami Amit máté mondOT
     </div>
 )
-/*
+
     return React.createElement(
   'div', {className:'App'},
   React.createElement('h1', null, 'React feladatok  2.0')
-)  )*/
+)  )
 
 }
 
+*/
 
 
 
 
 
-export default App;
