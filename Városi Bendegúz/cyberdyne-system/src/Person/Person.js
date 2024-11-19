d5f7b7edd5640d8341979dac9b591f3214393177
VarosiBendeguz <varosi.bendeguz19k@egressy.info>
2024-11-19 10:34:26 +0100

    kgl

--- Városi Bendegúz/cyberdyne-system/src/Person/Person.js
+++ Városi Bendegúz/cyberdyne-system/src/Person/Person.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React from "react"
+import './Person.css'
 
 const szemely = (props) => {
     return(
-    <div>
+    <div className='Person'>
         <p onClick={props.click}>Nevem: {props.name}, korom: {props.age}</p>
         <input type="text" onChange={props.change} value={props.name}></input>
     </div>
