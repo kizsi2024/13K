8b2972e1b337e6d23f7f78612c42c173eb886b7e
VarosiBendeguz <varosi.bendeguz19k@egressy.info>
2025-03-04 08:49:38 +0100

    gh

--- Városi Bendegúz/cyberdyne-system/src/App.js
+++ Városi Bendegúz/cyberdyne-system/src/App.js
@@ -26,7 +26,7 @@ export default App;
 */
 
 
-/*
+
 import React, {useState} from 'react'
 import './App.css';
 import Person from './Person/Person'
@@ -38,7 +38,7 @@ const App = props=> {
       {name: "János", age: 58},
       {name: "Kocka Karesz", age: 20}
     ],
-    masikState: 'másik state'
+    //otherState: "Ez egy másik állpot"
   })
   
   
@@ -69,124 +69,44 @@ const App = props=> {
 }
 
 
-export default App;*/
+export default App;
 
 
 
-
-
-
-
-
-////////////////////////////////      SPREAD OPERATOR      //////////////////////////////////////////
-
-
-
-
-
+/*
 import React, {Component} from 'react'
-import './App.css'
+import './App.css';
 import Person from './Person/Person'
-import { eventWrapper } from '@testing-library/user-event/dist/utils';
 
 class App extends Component{
   
     state = {
       persons: [
-        {id:'dsfsf', name: "Hübele Balázs", age: 26},
-        {id:'uizui', name: "János", age: 58},
-        {id:'hmnm', name: "Kocka Karesz", age: 20}
-      ],
-      masikState: 'másik state',
-      lathatosag: true
+        {name: "Hübele Balázs", age: 26},
+        {name: "János", age: 58},
+        {name: "Kocka Karesz", age: 20}
+      ]
     }
 
-    nameChangeHandler = (ujNev) => {
+    nameChangeHandler = () => {
       this.setState({
         persons: [
-          {name: ujNev, age: 26},
+          {name: "Hübele Balázs", age: 26},
           {name: "János", age: 34},
           {name: "Kocka Károly", age: 20}
         ]
       })
     }
-
-    nameValtozasHandler = (event, id) => {
-      const szemelyIndex = this.state.persons.findIndex(aktSzemely => {
-        return aktSzemely.id === id
-      })
-
-      const szemely = {
-        ...this.state.persons[szemelyIndex]
-      }
-
-      szemely.name = event.target.value
-      const szemelyek = [...this.state.persons]
-      szemelyek[szemelyIndex] = szemely
-
-        this.setState({
-          persons: szemelyek
-        })
-
-    }
-
-    kapcsoloHandler = () => {
-      const lathato = this.state.lathatosag
-      this.setState({lathatosag: !lathato})
-    }
-
-    personDeleteHandler = (personIndex) => {
-      //const persons = this.state.persons
-      //persons.splice(personIndex, 1)
-      //this.setState({persons: persons})
-      const szemelyek = [...this.state.persons]
-      szemelyek.splice(personIndex, 1)
-      this.setState({persons: szemelyek})
-    }
   
   render() {
-
-    const stilus = {
-      backgroundColor: 'green',
-      font: 'inherit',
-      border: '2px solid green',
-      padding: '8px',
-      cursor: 'pointer',
-      color: 'white'
-    }
-
-    let persons = null
-    if(this.state.lathatosag){
-      persons = (
-        
-        <div>
-        
-        {
-          this.state.persons.map((person, index) => {
-            return <Person
-
-              name={person.name}
-              age={person.age}
-              delete={() => this.personDeleteHandler(index)}
-              key={person.id}
-              change={(event) => this.nameValtozasHandler(event, person.id)}
-            />
-          })
-        }
-
-
-        </div>
-      )
-      stilus.backgroundColor = 'red'
-    }
-
-
     return(
       <div className='App'>
-        <h1>Sziasztok</h1>
-        <p>Ez egy paragraf</p>
-        <button style={stilus} onClick={this.kapcsoloHandler}>Nevet módosit</button>
-        {persons}
+        <h1>React feladatok</h1>
+        <p>Ez egy bekezdés</p>
+        <button onClick={this.nameChangeHandler}>Nevet módosit</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     )
   
@@ -194,3 +114,4 @@ class App extends Component{
 }
 
 export default App;
+*/
